bb4fd39271292b99f3101e31cd07b7bbb6e1110f
Raymond Beaudoin <24757919+synackray@users.noreply.github.com>
2019-09-17 00:08:09 +0200

    Pylint cleanup

--- dns_load_gen.py
+++ dns_load_gen.py
@@ -13,19 +13,19 @@ class MyParser(argparse.ArgumentParser):
         sys.exit(2)
 
 class DNSQueryGenerator:
-    def __init__(self, count, destinations, record, type, verbose):
+    def __init__(self, count, destinations, record, rtype, verbose):
         self.count = count
         self.destinations = destinations
         self.record = record
-        self.type = type
+        self.rtype = rtype
         self.verbose = verbose
 
     def build_packet(self, record):
-        types = {
+        rtypes = {
             "a": 1, "cname": 5, "mx": 15, "ns": 2, "ptr": 12, "soa": 6,
             "srv": 33, "txt": 16
             }
-        packet = struct.pack(">H", random.randrange(1,65535))  # Query Ids
+        packet = struct.pack(">H", random.randrange(1, 65535))  # Query Ids
         packet += struct.pack(">H", 256) # Flags
         packet += struct.pack(">H", 1) # Questions
         packet += struct.pack(">H", 0) # Answers
@@ -34,17 +34,17 @@ class DNSQueryGenerator:
         split_record = record.split(".")
         for part in split_record:
             packet += struct.pack("B", len(part))
-            for byte in (part):
+            for byte in part:
                 packet += struct.pack("c", bytes(byte, "utf-8"))
         packet += struct.pack("B", 0) # End of String
-        packet += struct.pack(">H", types[self.type]) # Query Type
+        packet += struct.pack(">H", rtypes[self.rtype]) # Record Type
         packet += struct.pack(">H", 1) # Query Class
         return packet
 
     def send_queries(self, work_queue):
         count = 0
         skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        skt.bind(("",5000))
+        skt.bind(("", 5000))
         for dest in self.destinations:
             for item in work_queue:
                 skt.sendto(bytes(item), (str(dest), 53))
@@ -52,7 +52,7 @@ class DNSQueryGenerator:
         if self.verbose:
             sys.stdout.write(
                 f"\rSent {int(count/len(self.destinations))} queries for "
-                f"{self.type.upper()} records of"
+                f"{self.rtype.upper()} records of"
                 f" {self.record} to {len(self.destinations)} destinations. "
                 f"Total of {count} queries sent.\n"
                 )
@@ -75,33 +75,38 @@ class DNSQueryGenerator:
 
 def main():
     parser = MyParser()
-    parser.add_argument("-c", "--count", default="10", metavar="10", type=int,
+    parser.add_argument(
+        "-c", "--count", default="10", metavar="10", type=int,
         help="Set how many queries will be generated. "
              "Default: 10"
         )
-    parser.add_argument("-d", "--destinations", metavar="192.0.2.1", nargs="+",
+    parser.add_argument(
+        "-d", "--destinations", metavar="192.0.2.1", nargs="+",
         required=True, type=str,
         help="Set DNS server(s) to query. Accepts multiple entries "
              "separated by space."
         )
-    parser.add_argument("-r", "--record", metavar="example.com",
+    parser.add_argument(
+        "-r", "--record", metavar="example.com",
         required=True, type=str,
         help="Set name of the record to query."
         )
-    parser.add_argument("-t", "--type",
+    parser.add_argument(
+        "-t", "--type",
         choices=["a", "cname", "mx", "ns", "ptr", "soa", "srv", "txt"],
         default="a", type=str,
-        help="Set type record to send query DNS servers for. "
+        help="Set type of record to query the DNS servers for. "
              "Default: A Record"
         )
-    parser.add_argument("-v", "--verbose", action="store_true",
+    parser.add_argument(
+        "-v", "--verbose", action="store_true",
         help="Provide more detailed output."
         )
     args = parser.parse_args()
 
     return DNSQueryGenerator(
         count=args.count, destinations=args.destinations, record=args.record,
-            type=args.type, verbose=args.verbose
+        rtype=args.type, verbose=args.verbose
         ).queue_handler()
 
 
